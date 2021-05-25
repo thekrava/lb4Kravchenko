24b4b9208eb2e5188dea50812ed709061b598d6d
Кравченко Дмитро <cravchenko5@ukr.net>
2021-05-25 14:53:42 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+namespace Task2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Title = "Вираховуємо";
+            double x = (double)new Random().Next() / int.MaxValue * (2 * Math.PI);
+            Console.WriteLine($"x= {x}");
+            Console.WriteLine("W= 1+sin(x)-2*cos(x)+4*sin2(x)-8*cos2(x)+...-...=");
+            Console.Write("= 1");
+            Console.CancelKeyPress += CtrlBrec;
+            ThreadPool.QueueUserWorkItem(Calculate, x, true);
+            Console.ReadKey(true);
+            if (!_exit)
+            {
+                _exit = true;
+                lock (_con) Console.WriteLine();
+            }
+        }
+        static void CtrlBrec(object sender, ConsoleCancelEventArgs e)
+        {
+            _exit = true;
+            lock (_con) Console.WriteLine();
+        }
+        static readonly object _con = new();
+        static volatile bool _exit = false;
+        static void Calculate(double x)
+        {
+            double result = 1;
+            for (int i = 0; ; i++)
+            {
+                if ((i & 1) == 0)
+                    result += (1 << i) * Math.Sin(x);
+                else
+                    result -= (1 << i) * Math.Cos(x);
+                if (_exit) return;
+                lock (_con)
+                {
+                    Console.CursorLeft = 2;
+                    Console.Write($"{result,-21}");
+                }
+                Thread.Sleep(17);
+            }
+        }
+    }
+}
